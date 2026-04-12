86240c447323528b2591240b741319834e8e4524
Admin <furqanfurqan6677@gmail.com>
2026-04-12 21:28:57 +0530

    Added Rect

--- apps/web/app/signup/page.tsx
+++ apps/web/app/signup/page.tsx
@@ -16,58 +16,75 @@ export default function SignupPage() {
     return <div style={{
         display: "flex",
         justifyContent: "center",
-        alignItems: "center"
+        alignItems: "center",
+        minHeight: "100vh",
+        backgroundColor: "#f5f5f5"
     }}>
         <div style={{
-            marginTop: 100
+            backgroundColor: "white",
+            padding: "40px",
+            borderRadius: "15px",
+            boxShadow: "0 2px 10px rgba(0,0,0,0.1)",
+            width: "100%",
+            maxWidth: "400px"
         }}>
 
             <div style={{
                 display: "flex",
                 justifyContent: "center",
-                fontSize: 24,
-                fontWeight: 600
+                fontSize: 28,
+                fontWeight: 700,
+                marginBottom: 30,
+                color: "black"
             }}>
-                <p>Signup </p>
+                <p>Sign Up</p>
             </div>
-
-
 
             <div style={{
                 display: "flex",
-                flexDirection: "column"
+                flexDirection: "column",
+                gap: "15px"
             }}>
                 <input style={{
-                    marginTop: 20,
-                    height: 35,
-                    borderRadius: 10,
-                    paddingLeft: 10,
+                    height: 50,
+                    borderRadius: 8,
+                    paddingLeft: 15,
+                    paddingRight: 15,
                     borderColor: "black",
-                    borderWidth: 1,
-                    width: 230
-                }} type="text" placeholder="Enter Name " onChange={(e) => {
+                    borderWidth: 2,
+                    width: "100%",
+                    fontSize: 16,
+                    boxSizing: "border-box",
+                    fontFamily: "inherit"
+                }} type="text" placeholder="Enter name" onChange={(e) => {
                     setName(e.target.value);
                 }} />
                 <input style={{
-                    marginTop: 20,
-                    height: 35,
-                    borderRadius: 10,
-                    paddingLeft: 10,
+                    height: 50,
+                    borderRadius: 8,
+                    paddingLeft: 15,
+                    paddingRight: 15,
                     borderColor: "black",
-                    borderWidth: 1,
-                    width: 230
-                }} type="email" placeholder="Enter email " onChange={(e) => {
+                    borderWidth: 2,
+                    width: "100%",
+                    fontSize: 16,
+                    boxSizing: "border-box",
+                    fontFamily: "inherit"
+                }} type="email" placeholder="Enter email" onChange={(e) => {
                     setEmail(e.target.value);
                 }} />
                 <input style={{
-                    marginTop: 20,
-                    height: 35,
-                    borderRadius: 10,
-                    paddingLeft: 10,
+                    height: 50,
+                    borderRadius: 8,
+                    paddingLeft: 15,
+                    paddingRight: 15,
                     borderColor: "black",
-                    borderWidth: 1,
-                    width: 230
-                }} type="password" placeholder="Enter password " onChange={(e) => {
+                    borderWidth: 2,
+                    width: "100%",
+                    fontSize: 16,
+                    boxSizing: "border-box",
+                    fontFamily: "inherit"
+                }} type="password" placeholder="Enter password" onChange={(e) => {
                     setPassword(e.target.value);
                 }} />
             </div>
@@ -75,21 +92,23 @@ export default function SignupPage() {
             <div style={{
                 display: "flex",
                 justifyContent: "center",
-                color: "white",
-                marginTop: 20
+                color: "black",
+                marginTop: 25,
+                fontSize: 14
             }}>
-                <p>Already have an account ? <span style={{
-                    cursor: "pointer"
+                <p>Already have an account? <span style={{
+                    cursor: "pointer",
+                    fontWeight: 600,
+                    textDecoration: "underline"
                 }} onClick={() => {
                     Navigate.push("/signin")
-                }}>Signin</span></p>
+                }}>Sign in</span></p>
             </div>
-
 
             <div style={{
                 display: "flex",
                 justifyContent: "center",
-                marginTop: 20
+                marginTop: 25
             }}>
                 <button onClick={async () => {
                     if (name != "" && email != "" && password != "") {
@@ -110,16 +129,18 @@ export default function SignupPage() {
                         alert("Please fill all the fields");
                     }
                 }} style={{
-                    fontSize: 18,
-                    borderRadius: 10,
-                    padding: 4,
+                    fontSize: 16,
+                    fontWeight: 600,
+                    borderRadius: 8,
+                    padding: "12px 32px",
                     borderColor: "black",
-                    borderWidth: 1
-                }}>Signup</button>
+                    borderWidth: 2,
+                    backgroundColor: "black",
+                    color: "white",
+                    cursor: "pointer",
+                    transition: "all 0.3s ease"
+                }}>Sign Up</button>
             </div>
-
-
-
 
         </div>
     </div>
